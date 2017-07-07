a35c9125fccdee5e3a16060802f9a8d4461a9ad5
BrunoReis <brunoreis@mail.com>
2017-07-07 18:09:28 +0000

    Estrutura do Jogo

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngMap'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,81 +26,313 @@ angular.module('starter', ['ionic'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-
+//Apresentação
   .state('apresentacao', {
     url: '/apresentacao',
     templateUrl: '/templates/apresentacao.html',
     controller: 'ApresentacaoCtrl',
   })
 
+//Login
   .state('login', {
     url: '/login',
     templateUrl: '/templates/login.html',
     controller: 'LoginCtrl',
   })
 
-  .state('historico', {
-    url: '/historico',
-    templateUrl: '/templates/historico.html',
-    controller: 'HistoricoCtrl',
+//Instruções
+  .state('instrucoes', {
+    url: '/instrucoes',
+    templateUrl: '/templates/instrucoes.html',
+    controller: 'InstrucoesCtrl',
   })
 
-  .state('inicio', {
-    url: '/inicio',
-    templateUrl: '/templates/inicio.html',
-    controller: 'InicioCtrl',
+//Opções de história
+  .state('opcoes', {
+    url: '/opcoes',
+    templateUrl: '/templates/opcoes.html',
+    controller: 'OpcoesCtrl',
   })
 
-  .state('detalhes1', {
-    url: '/detalhes1',
-    templateUrl: '/templates/detalhes1.html',
-    controller: 'Detalhes1Ctrl',
+//Opções Civil
+.state('opCivil', {
+  url: '/opCivil',
+  templateUrl: '/templates/opCivil.html',
+  controller: 'OpCivilCtrl',
+})
+
+//RICARDO (MILITARY)///////////////////////////////////////
+
+//Capa Ricardo
+.state('capaRicardo', {
+  url: '/capaRicardo',
+  templateUrl: '/templates/capaRicardo.html',
+  controller: 'CapaRicardoCtrl',
+})
+
+//Intro Ricardo
+  .state('introRicardo', {
+    url: '/introRicardo',
+    templateUrl: '/templates/introRicardo.html',
+    controller: 'IntroRicardoCtrl',
   })
 
-  .state('missao', {
-    url: '/missao',
-    templateUrl: '/templates/missao.html',
-    controller: 'MissaoCtrl',
+//Mapa Ricardo
+  .state('mapaRicardo', {
+    url: '/mapaRicardo',
+    templateUrl: '/templates/mapaRicardo.html',
+    controller: 'MapaRicardoCtrl',
   })
 
-  .state('sucesso', {
-    url: '/sucesso',
-    templateUrl: '/templates/sucesso.html',
-    controller: 'SucessoCtrl',
+//Ricardo Situação 1 (situação inicial tensa)
+  .state('ricardo1', {
+    url: '/ricardo1',
+    templateUrl: '/templates/ricardo1.html',
+    controller: 'Ricardo1Ctrl',
   })
 
-  .state('fracasso', {
-    url: '/fracasso',
-    templateUrl: '/templates/fracasso.html',
-    controller: 'FracassoCtrl',
+//Ricardo Situ 2 (violenta)
+  .state('ricardo2', {
+    url: '/ricardo2',
+    templateUrl: '/templates/ricardo2.html',
+    controller: 'Ricardo2Ctrl',
   })
 
-
-  .state('recuso', {
-    url: '/recuso',
-    templateUrl: '/templates/recuso.html',
-    controller: 'RecusoCtrl',
+//Ricardo Situ 3 (paz/inteligência)
+  .state('ricardo3', {
+    url: '/ricardo3',
+    templateUrl: '/templates/ricardo3.html',
+    controller: 'Ricardo3Ctrl',
   })
 
-  .state('detalhes2', {
-    url: '/detalhes2',
-    templateUrl: '/templates/detalhes2.html',
-    controller: 'Detalhes2Ctrl',
+//Ricardo Situ 4
+  .state('ricardo4', {
+    url: '/ricardo4',
+    templateUrl: '/templates/ricardo4.html',
+    controller: 'Ricardo4Ctrl',
   })
 
-
-  .state('missao2', {
-    url: '/missao2',
-    templateUrl: '/templates/missao2.html',
-    controller: 'Missao2Ctrl',
+//Ricardo Situ 5
+  .state('ricardo5', {
+    url: '/ricardo5',
+    templateUrl: '/templates/ricardo5.html',
+    controller: 'Ricardo5Ctrl',
   })
 
+//Ricardo Situ 6
+  .state('ricardo6', {
+    url: '/ricardo6',
+    templateUrl: '/templates/ricardo6.html',
+    controller: 'Ricardo6Ctrl',
+  })
 
-  .state('expulso', {
-    url: '/expulso',
-    templateUrl: '/templates/expulso.html',
-    controller: 'ExpulsoCtrl',
-  });
+//Ricardo Situ 7
+  .state('ricardo7', {
+    url: '/ricardo7',
+    templateUrl: '/templates/ricardo7.html',
+    controller: 'Ricardo7Ctrl',
+  })
+
+//Ricardo Situ 8
+    .state('ricardo8', {
+      url: '/ricardo8',
+      templateUrl: '/templates/ricardo8.html',
+      controller: 'Ricardo8Ctrl',
+    })
+
+//Ricardo Situ 9
+    .state('ricardo9', {
+      url: '/ricardo9',
+      templateUrl: '/templates/ricardo9.html',
+      controller: 'Ricardo9Ctrl',
+    })
+
+//Ricardo Situ 10
+    .state('ricardo10', {
+        url: '/ricardo10',
+        templateUrl: '/templates/ricardo10.html',
+        controller: 'Ricardo10Ctrl',
+    })
+
+//Ricardo Situ 11
+    .state('ricardo11', {
+      url: '/ricardo11',
+      templateUrl: '/templates/ricardo11.html',
+      controller: 'Ricardo11Ctrl',
+    })
+
+//Ricardo Situ 12
+    .state('ricardo12', {
+        url: '/ricardo12',
+        templateUrl: '/templates/ricardo12.html',
+        controller: 'Ricardo12Ctrl',
+    })
+
+//Ricardo Situ 13
+    .state('ricardo13', {
+      url: '/ricardo13',
+      templateUrl: '/templates/ricardo13.html',
+      controller: 'Ricardo13Ctrl',
+    })
+
+//Ricardo Situ 14
+    .state('ricardo14', {
+      url: '/ricardo14',
+      templateUrl: '/templates/ricardo14.html',
+      controller: 'Ricardo14Ctrl',
+    })
+
+//Ricardo Situ 15
+    .state('ricardo15', {
+      url: '/ricardo15',
+      templateUrl: '/templates/ricardo15.html',
+      controller: 'Ricardo15Ctrl',
+    })
+
+//IGOR (INTELLIGENCE)////////////////////////////////////////////////////////
+//Intro Igor
+  .state('introIgor', {
+    url: '/introIgor',
+    templateUrl: '/templates/introIgor.html',
+    controller: 'IntroIgorCtrl',
+  })
+
+//Capa Igor
+  .state('capaIgor', {
+    url: '/capaIgor',
+    templateUrl: '/templates/capaIgor.html',
+    controller: 'CapaIgorCtrl',
+  })
+
+//Mapa Intel
+  .state('mapaIgor', {
+    url: '/mapaIgor',
+    templateUrl: '/templates/mapaIgor.html',
+    controller: 'MapaIgorCtrl',
+  })
+
+//Intel Situação 1 (situação inicial tensa)
+  .state('igor1', {
+    url: '/igor1',
+    templateUrl: '/templates/igor1.html',
+    controller: 'Igor1Ctrl',
+  })
+
+  //Igor Situ 2 (violenta)
+    .state('igor2', {
+      url: '/igor2',
+      templateUrl: '/templates/igor2.html',
+      controller: 'Igor2Ctrl',
+    })
+
+  //Igor Situ 3 (paz/inteligência)
+    .state('igor3', {
+      url: '/igor3',
+      templateUrl: '/templates/igor3.html',
+      controller: 'Igor3Ctrl',
+    })
+
+  //Igor Situ 4
+    .state('igor4', {
+      url: '/igor4',
+      templateUrl: '/templates/igor4.html',
+      controller: 'Igor4Ctrl',
+    })
+
+  //Igor Situ 5
+    .state('igor5', {
+      url: '/igor5',
+      templateUrl: '/templates/igor5.html',
+      controller: 'Igor5Ctrl',
+    })
+
+  //Igor Situ 6
+    .state('igor6', {
+      url: '/igor6',
+      templateUrl: '/templates/igor6.html',
+      controller: 'Igor6Ctrl',
+    })
+
+  //Igor Situ 7
+    .state('igor7', {
+      url: '/igor7',
+      templateUrl: '/templates/igor7.html',
+      controller: 'Igor7Ctrl',
+    })
+
+  //Igor Situ 8
+      .state('igor8', {
+        url: '/igor8',
+        templateUrl: '/templates/igor8.html',
+        controller: 'Igor8Ctrl',
+      })
+
+  //Igor Situ 9
+      .state('igor9', {
+        url: '/igor9',
+        templateUrl: '/templates/igor9.html',
+        controller: 'Igor9Ctrl',
+      })
+
+  //Igor Situ 10
+      .state('igor10', {
+          url: '/igor10',
+          templateUrl: '/templates/igor10.html',
+          controller: 'Igor10Ctrl',
+      })
+
+  //Igor Situ 11
+      .state('igor11', {
+        url: '/igor11',
+        templateUrl: '/templates/igor11.html',
+        controller: 'Igor11Ctrl',
+      })
+
+  //Igor Situ 12
+      .state('igor12', {
+          url: '/igor12',
+          templateUrl: '/templates/igor12.html',
+          controller: 'Igor12Ctrl',
+      })
+
+  //Igor Situ 13
+      .state('igor13', {
+        url: '/igor13',
+        templateUrl: '/templates/igor13.html',
+        controller: 'Igor13Ctrl',
+      })
+
+  //Igor Situ 14
+      .state('igor14', {
+        url: '/igor14',
+        templateUrl: '/templates/igor14.html',
+        controller: 'Igor14Ctrl',
+      })
+
+  //Igor Situ 15
+      .state('igor15', {
+        url: '/igor15',
+        templateUrl: '/templates/igor15.html',
+        controller: 'Igor15Ctrl',
+      })
+
+//REBEL/////////////////////////////////////////////////////////
+//Intro REBEL
+.state('introRebel', {
+  url: '/introRebel',
+  templateUrl: '/templates/introRebel.html',
+  controller: 'IntroRebelCtrl',
+})
+
+//JOURNALIST/////////////////////////////////////////////////////////
+//Intro JOURNALIST
+.state('introJournal', {
+  url: '/introJournal',
+  templateUrl: '/templates/introJournal.html',
+  controller: 'IntroJournalCtrl',
+})
+
+
 
 
   // if none of the above states are matched, use this as the fallback
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,90 +9,422 @@ angular.module('starter')
 
 
 .controller('LoginCtrl', function($scope, $state) {
-  $scope.historico = function() {
-    $state.go('historico')
+  $scope.instrucoes = function() {
+    $state.go('instrucoes')
   }
 })
 
 
-.controller('HistoricoCtrl', function($scope, $state) {
-  $scope.inicio = function() {
-    $state.go('inicio')
+.controller('InstrucoesCtrl', function($scope, $state) {
+  $scope.opcoes = function() {
+    $state.go('opcoes')
+  }
+})
+
+//Menu Opções Controller
+.controller('OpcoesCtrl', function($scope, $state) {
+  $scope.capaRicardo = function() {
+    $state.go('capaRicardo')
+  }
+  $scope.capaIgor = function() {
+    $state.go('capaIgor')
+  }
+  $scope.opCivil = function() {
+    $state.go('opCivil')
+  }
+})
+
+//Opções Civil
+.controller('OpCivilCtrl', function($scope, $state) {
+  $scope.introRebel = function() {
+    $state.go('introRebel')
+  }
+  $scope.introJournal = function() {
+    $state.go('introJournal')
+  }
+})
+
+//RICARDO (MILITARY)///////////////////////////////////////////////////////////
+
+//Capa Ricardo
+.controller('CapaRicardoCtrl', function($scope, $state){
+  $scope.introRicardo = function() {
+    $state.go('introRicardo')
+  }
+})
+//Intro Ricardo
+.controller('IntroRicardoCtrl', function($scope, $state){
+  $scope.mapaRicardo = function() {
+    $state.go('mapaRicardo')
+  }
+})
+
+//Mapa Ricardo
+.controller('MapaRicardoCtrl', function($scope, $state){
+  $scope.ricardo1 = function() {
+    $state.go('ricardo1')
   }
 })
 
 
-.controller('InicioCtrl', function($scope, $state) {
-  $scope.detalhes1 = function() {
-    $state.go('detalhes1')
+//Ricardo Situ 1 Controller
+.controller('Ricardo1Ctrl', function($scope, $state) {
+  $scope.ricardo2 = function() {
+    $state.go('ricardo2')
+  }
+  $scope.ricardo3 = function() {
+    $state.go('ricardo3')
   }
 })
 
-.controller('Detalhes1Ctrl', function($scope, $state){
-  $scope.missao = function() {
-    $state.go('missao')
+//Ricardo Situ 2 Controller
+.controller('Ricardo2Ctrl', function($scope, $state){
+  $scope.ricardo4 = function() {
+    $state.go('ricardo4')
   }
-
-  $scope.recuso = function() {
-    $state.go('recuso')
-  }
-})
-
-
-
-.controller('MissaoCtrl', function($scope, $state){
-  $scope.sucesso = function() {
-    $state.go('sucesso')
-  }
-
-  $scope.fracasso = function() {
-    $state.go('fracasso')
+  $scope.ricardo5 = function() {
+    $state.go('ricardo5')
   }
 })
 
-
-
-.controller('SucessoCtrl', function($scope, $state) {
-  $scope.historico = function() {
-    $state.go('historico')
+//Ricardo Situ 3 Controller
+.controller('Ricardo3Ctrl', function($scope, $state){
+  $scope.ricardo6 = function() {
+    $state.go('ricardo6')
+  }
+  $scope.ricardo7 = function() {
+    $state.go('ricardo7')
   }
 })
 
-
-.controller('FracassoCtrl', function($scope, $state){
-  $scope.inicio = function() {
-    $state.go('inicio')
-}
-})
-
-.controller('Missao2Ctrl', function($scope, $state){
-
-})
-
-
-.controller('Detalhes2Ctrl', function($scope, $state){
-  $scope.missao2 = function(){
-    $state.go('missao2')
+//Ricardo Situ 4 Controller
+.controller('Ricardo4Ctrl', function($scope, $state){
+  $scope.ricardo8 = function(){
+    $state.go('ricardo8')
   }
 
-  $scope.expulso = function(){
-    $state.go('expulso')
+  $scope.ricardo9 = function(){
+    $state.go('ricardo9')
   }
-
 })
 
+//Ricardo Situ 5 Controller
+.controller('Ricardo5Ctrl', function($scope, $state){
+  $scope.ricardo10 = function(){
+    $state.go('ricardo10')
+  }
 
-
-.controller('RecusoCtrl', function($scope, $state){
-  $scope.detalhes2 = function(){
-    $state.go('detalhes2')
+  $scope.ricardo11 = function(){
+    $state.go('ricardo11')
   }
 })
 
 
-.controller('ExpulsoCtrl', function($scope, $state){
-  $scope.historico = function(){
-    $state.go('historico')
+//Ricardo Situ 6 Controller
+.controller('Ricardo6Ctrl', function($scope, $state){
+  $scope.ricardo12 = function(){
+    $state.go('ricardo12')
+  }
+  $scope.ricardo13 = function(){
+    $state.go('ricardo13')
+  }
+})
+
+//Ricardo Situ 7 Controller
+.controller('Ricardo7Ctrl', function($scope, $state){
+  $scope.ricardo14 = function(){
+    $state.go('ricardo14')
+  }
+  $scope.ricardo15 = function(){
+    $state.go('ricardo15')
+  }
+})
+
+//Ricardo Situ 8 Controller
+.controller('Ricardo8Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 9 Controller
+.controller('Ricardo9Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 10 Controller
+.controller('Ricardo10Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 11 Controller
+.controller('Ricardo11Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 12 Controller
+.controller('Ricardo12Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 13 Controller
+.controller('Ricardo13Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 14 Controller
+.controller('Ricardo14Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Ricardo Situ 15 Controller
+.controller('Ricardo15Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//(INTELLIGENCE)////////////////////////////////////////////////////////
+
+//Capa Igor
+.controller('CapaIgorCtrl', function($scope, $state){
+  $scope.introIgor = function() {
+    $state.go('introIgor')
+  }
+})
+
+//Intro Igor
+.controller('IntroIgorCtrl', function($scope, $state){
+  $scope.mapaIgor = function() {
+    $state.go('mapaIgor')
+  }
+})
+
+//Mapa Igor
+.controller('MapaIgorCtrl', function($scope, $state){
+  $scope.igor1 = function() {
+    $state.go('igor1')
+  }
+})
+
+
+//Igor Situ 1 Controller
+.controller('Igor1Ctrl', function($scope, $state) {
+  $scope.igor2 = function() {
+    $state.go('igor2')
+  }
+  $scope.igor3 = function() {
+    $state.go('igor3')
+  }
+})
+
+//Igor Situ 2 Controller
+.controller('Igor2Ctrl', function($scope, $state){
+  $scope.igor4 = function() {
+    $state.go('igor4')
+  }
+  $scope.igor5 = function() {
+    $state.go('igor5')
+  }
+})
+
+//Igor Situ 3 Controller
+.controller('Igor3Ctrl', function($scope, $state){
+  $scope.igor6 = function() {
+    $state.go('igor6')
+  }
+  $scope.igor7 = function() {
+    $state.go('igor7')
+  }
+})
+
+//Igor Situ 4 Controller
+.controller('Igor4Ctrl', function($scope, $state){
+  $scope.igor8 = function(){
+    $state.go('igor8')
   }
 
-});
+  $scope.igor9 = function(){
+    $state.go('igor9')
+  }
+})
+
+//Igor Situ 5 Controller
+.controller('Igor5Ctrl', function($scope, $state){
+  $scope.igor10 = function(){
+    $state.go('igor10')
+  }
+
+  $scope.igor11 = function(){
+    $state.go('igor11')
+  }
+})
+
+
+//Igor Situ 6 Controller
+.controller('Igor6Ctrl', function($scope, $state){
+  $scope.igor12 = function(){
+    $state.go('igor12')
+  }
+  $scope.igor13 = function(){
+    $state.go('igor13')
+  }
+})
+
+//Igor Situ 7 Controller
+.controller('Igor7Ctrl', function($scope, $state){
+  $scope.igor14 = function(){
+    $state.go('igor14')
+  }
+  $scope.igor15 = function(){
+    $state.go('igor15')
+  }
+})
+
+//Igor Situ 8 Controller
+.controller('Igor8Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 9 Controller
+.controller('Igor9Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 10 Controller
+.controller('Igor10Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 11 Controller
+.controller('Igor11Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 12 Controller
+.controller('Igor12Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 13 Controller
+.controller('Igor13Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 14 Controller
+.controller('Igor14Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//Igor Situ 15 Controller
+.controller('Igor15Ctrl', function($scope, $state){
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+  $scope.opcoes = function(){
+    $state.go('opcoes')
+  }
+})
+
+//(CIVILIAN - REBEL)////////////////////////////////////////////////////////
+//Intro Rebel
+.controller('IntroRebelCtrl', function($scope, $state){
+
+  })
+
+//Mapa Rebel
+.controller('MapaRebelCtrl', function($scope, $state){
+
+})
+
+//Rebel Situação 1
+.controller('Rebel1Ctrl', function($scope, $state){
+
+})
+
+//(CIVILIAN - JOURNALIST)////////////////////////////////////////////////////////
+//Intro Journal
+.controller('IntroJournalCtrl', function($scope, $tate){
+
+  })
+
+//Mapa Journal
+.controller('MapaJournalCtrl', function($scope, $state){
+
+})
+
+//Journal Situação 1
+.controller('Journal1Ctrl', function($scope, $state){
+
+})
